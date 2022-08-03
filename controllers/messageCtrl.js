0d507a3a4daea444109b97ca7b3fa95b1afcf2c5
rjhelm <rjhelm@github.io>
2022-08-03 16:58:06 -0600

    finished auth controller and comment controller || started messageCTRL

---
+++ controllers/messageCtrl.js
@@ -0,0 +1,23 @@
+const Conversations = require("../models/conversationModel");
+const Messages = require("../models/messageModel");
+
+class APIfeatures {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    paginating() {
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 9;
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit);
+        return this;
+    }
+}
+
+const messageCtrl = {
+    createMessage: async (req, res) => {
+        
+    }
+}
