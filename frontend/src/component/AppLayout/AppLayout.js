41f0586b3e7960a726b2f3b09936f2fffc1e645e
rjhelm <rjhelm@github.io>
2022-08-04 16:20:25 -0600

    altered the frontend a bit and saved backend due to changes

---
+++ frontend/src/component/AppLayout/AppLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AppLayout'
+import HeaderNav from '../../containers/HeaderNav/HeaderNav'
+import ScrollToTop from '../ScrollToTop/ScrollToTop'
+
+const AppLayout = (props) => {
+    return (
+        <ScrollToTop>
+            <div className="app-layout">
+                <HeaderNav />
+                {props.children}
+            </div>
+        </ScrollToTop>
+    );
+}
+
+export default AppLayout;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/global.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import DataProvider from './redux/store';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
