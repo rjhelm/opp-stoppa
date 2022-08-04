3adb2f4758728373d97c4008b5d631f43b2fb049
rjhelm <rjhelm@github.io>
2022-08-03 20:02:46 -0600

    finished socket server setup

--- server.js
+++ server.js
@@ -1,49 +1,53 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const SocketServer = require('./socketServer');
-
 const corsOptions = {
-    Credential: 'true',
-
+  Credential: 'true',
+  
 };
+
 
 const app = express();
 
 app.use(express.json())
-app.options("*", cors(corsOptions));
+app.options("*" , cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(cookieParser())
 
-// #region // !Socket
+ // !Socket
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+
 
 io.on('connection', socket => {
     SocketServer(socket);
 })
 
-// #end region
+// !Routes
+app.use('/api', require('./routes/authRouter'));
+app.use('/api', require('./routes/userRouter'));
+app.use('/api', require('./routes/postRouter'));
+app.use('/api', require('./routes/commentRouter'));
+app.use('/api', require('./routes/adminRouter'));
+app.use('/api', require('./routes/notifyRouter'));
+app.use('/api', require('./routes/messageRouter'));
 
-// #region // !Routes
-
-
-// #end region
-
-const URI = process.env.MONGODB_URL_test;
+const URI = process.env.MONGODB_URL;
 mongoose.connect(URI, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
 }, err => {
     if(err) throw err;
-    console.log("Database Connected")
+    console.log("Database Connected!!")
 })
 
 const port = process.env.PORT || 8080;
 http.listen(port, () => {
-    console.log("Listening on ", port);
+  console.log("Listening on ", port);
 });
