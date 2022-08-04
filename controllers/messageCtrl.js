59aee2c8a4443d65eab87654b8ede99e5e42456d
rjhelm <rjhelm@github.io>
2022-08-03 19:46:18 -0600

    created controller for conversations

--- controllers/messageCtrl.js
+++ controllers/messageCtrl.js
@@ -18,6 +18,86 @@ class APIfeatures {
 
 const messageCtrl = {
     createMessage: async (req, res) => {
-        
-    }
-}
+        try {
+            const { recipient, text, media } = req.body;
+            if (!recipient || (!text.trim() && media.length === 0)) return;
+
+            const newConversation = await Conversations.findOneAndUpdate(
+                {
+                    $or: [
+                        { recipients: [req.user._id, recipient] },
+                        { recipients: [recipient, req.user._id] },
+                    ],
+                },
+                {
+                    recipients: [req.user._id, recipient],
+                    text,
+                    media,
+                },
+                { new: true, upsert: true }
+            );
+
+            const newMessage = new Messages({
+                conversation: newConversation._id,
+                sender: req.user._id,
+                recipient,
+                text,
+                media,
+            });
+
+            await newMessage.save();
+
+            res.json({ msg: "Created." });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+
+    getConversations: async (req, res) => {
+        try {
+            const features = new APIfeatures(
+                Conversations.find({
+                    recipients: req.user._id,
+                }),
+                req.query
+            ).paginating();
+
+            const conversations = await features.query
+                .sort("-updatedAt")
+                .populate("recipients", "avatar username fullname");
+
+            res.json({
+                conversations,
+                result: conversations.length,
+            });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+
+    getMessages: async (req, res) => {
+        try {
+            const features = new APIfeatures(
+                Messages.find({
+                    $or: [
+                        { sender: req.user._id, recipient: req.params.id },
+                        { sender: req.params.id, recipient: req.user._id },
+                    ],
+                }),
+                req.query
+            ).paginating();
+
+            const messages = await features.query
+                .sort("-createdAt");
+
+            res.json({
+                messages,
+                result: messages.length,
+            });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+};
+
+module.exports = messageCtrl;
