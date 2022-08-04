d59636e9ff3a89aa6c7a0259172cc401ce0c8719
rjhelm <rjhelm@github.io>
2022-08-03 19:59:54 -0600

    finished controllers for post | users | completed routes for controllers

---
+++ routes/authRouter.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const authCtrl = require('../controllers/authCtrl');
+const auth = require('../middleware/auth');
+
+router.post('/register', authCtrl.register);
+router.post('/register_admin', authCtrl.registerAdmin);
+router.post('/changePassword', auth, authCtrl.changePassword);
+router.post('/login', authCtrl.login);
+router.post('/admin_login', auth, authCtrl.adminLogin);
+router.post('/logout', authCtrl.logout);
+router.post('/refresh_token', authCtrl.generateAccessToken);
+
+module.exports = router;
