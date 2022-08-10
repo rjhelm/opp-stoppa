eb501389e0d6232066ed5b7d2666935c0e90b683
rjhelm <rjhelm@github.io>
2022-08-09 19:46:00 -0600

    created container for comments with tests and styling

---
+++ frontend/src/containers/Comments/Comment/Comment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Comment.scss';
+import { Button, Image } from 'semantic-ui-react';
+import { Rating } from '../../../component/Rating/Rating';
+
+export const Comment = (props) => {
+    if (!props.comment) {
+        return <div />;
+    }
+    const topLevelComment = props.comment.snippet.topLevelComment;
+    const { authorProfileImageUrl, authorDisplayName, textOriginal } = topLevelComment.snippet;
+    const likeCount = topLevelComment.snippet.likeCount;
+
+    return (
+        <div className='comment'>
+            <Image className='user-image' src={authorDisplayName} circular />
+            <div>
+                <div className='user-name'>{authorDisplayName}</div>
+                <span>{textOriginal}</span>
+                <div className='comment-actions'>
+                    <Rating likeCount={likeCount} />
+                    <Button size='mini' compact>REPLY</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
