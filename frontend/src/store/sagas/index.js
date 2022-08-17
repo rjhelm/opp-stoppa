84938ea1e6bc8cf833e7e07ae23351c45d164086
rjhelm <rjhelm@github.io>
2022-08-17 15:55:36 -0600

    finished store

---
+++ frontend/src/store/sagas/index.js
@@ -0,0 +1,32 @@
+import { all, call, put, fork } from 'react-redux';
+import { watchMostPopularVideos, watchMostPopularVideosByCategory, watchVideoCategories } from './video';
+import { watchWatchDetails } from './watch';
+import { watchCommentThread } from './comment';
+import { watchSearchForVideos } from './search';
+
+export default function* () {
+    yield all([
+        fork(watchMostPopularVideos),
+        fork(watchVideoCategories),
+        fork(watchMostPopularVideosByCategory),
+        fork(watchWatchDetails),
+        fork(watchCommentThread),
+        fork(watchSearchForVideos)
+    ]);
+}
+
+export function* fetchEntity(request, entity, ...args) {
+    try {
+        const response = yield call(request);
+        yield put(entity.success(response.result, ...args));
+    } catch (error) {
+        yield put(entity.failure(error, ...args));
+    }
+}
+
+export const ignoreErrors = (fn, ...args) => {
+    return () => {
+        const ignoreErrorCallback = (response) => response;
+        return fn(...args).then(ignoreErrorCallback, ignoreErrorCallback);
+    }
+}
